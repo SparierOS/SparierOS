571270dfafc67749999db0e68078f72bf695e568
Roland Bernard <rolli.bernard@gmail.com>
2021-04-04 16:03:30 +0200

    Added trap handling and syscalls

--- src/main.rs
+++ src/main.rs
@@ -10,14 +10,20 @@
 
 // the entry point is in `mod cpu`
 
-mod bsp;
+#[macro_use]
 mod console;
+
+mod bsp;
 mod cpu;
 mod driver;
 mod memory;
 mod panic;
 mod runtime;
 mod synchronization;
+mod syscall;
+
+/// This is where the register will be safed on traps in kernel mode
+static mut KERNEL_REGISTERS: [usize; 31] =  [0; 31];
 
 /// Kernel initialization code.
 ///
@@ -25,6 +31,7 @@ mod synchronization;
 ///
 /// - Only one hart must be active
 unsafe fn kernel_init() -> ! {
+    cpu::setup_trap_vector(KERNEL_REGISTERS.as_mut_ptr()); // Setup trap handlers before anything that could cause exceptions
     driver::init();
 
     kernel_main()
@@ -44,9 +51,42 @@ fn kernel_main() -> ! {
         }
     }
 
-    println!("Hello World!");
-
-    // Safety: this code does not interfere with anything else
-    unsafe { asm!("1: wfi \n j 1b") };
-    unreachable!()
+    // Safety: depends on the implementation of enter_into_usermode
+    unsafe {
+        DEMO_REGISTERS[1] = DEMO_STACK.as_mut_ptr().add(DEMO_STACK.len() - 1) as usize; // Setup stack
+        DEMO_REGISTERS[2] = 0; // Global pointer will hopefully not be used
+        let entry_pc = user_demo as fn() as usize;
+        println!("[+] Entering usermode at {:#x}", entry_pc);
+        cpu::enter_into_usermode(DEMO_REGISTERS.as_mut_ptr(), entry_pc);
+    }
 }
+
+/// Handles traps. Gets called by the trap handler in 'traps.S'
+#[no_mangle]
+pub extern "C" fn kernel_trap_handler(registers: *mut usize, pc: usize, cause: u64, previous_mode: u64) -> ! {
+    if cause == 8 && previous_mode == 0 { // User Syscall
+        // Safety:
+        // - registers is always a value previously already written to from index 0 to 31
+        // - everything will be initialized because we come from usermode
+        unsafe {
+            syscall::handle_system_call(registers.add(9).read_volatile(), registers);
+            cpu::return_from_trap(registers, pc + 4); // reenter usermode after the ecall instruction
+        }
+    }
+    panic!("Unhandled trap {:#x} {:#x} {:#x}", pc, cause, previous_mode);
+}
+
+// This part would be part of process handling / program loading
+
+static mut DEMO_STACK: [usize; 1024] = [0; 1024];
+static mut DEMO_REGISTERS: [usize; 31] =  [0; 31];
+
+fn user_demo() {
+    // Safety: depends on the implementation of syscall and the trap and syscall handeling
+    unsafe {
+        cpu::syscall(0); // Print 'Hello world!'
+        cpu::syscall(1, 2 + 3); // Print integer
+        cpu::syscall(2); // Exit
+    }
+}
+
